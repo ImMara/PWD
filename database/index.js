2f80c16854ce59ce0dc6798a327e16dfb67c81f3
Mara <alaneraerts@live.be>
2021-04-11 18:55:54 +0200

    database & node

--- database/index.js
+++ database/index.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 exports.clientPromise = mongoose.connect('mongodb+srv://admin:WHNzzWyJHlmUEv9c@nogamenolife.eqdfq.mongodb.net/nogame?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then( () => console.log('CONNECT DB SUCCESS')).catch( err => console.log(err));
+}).then(() => console.log('CONNECT DB SUCCESS')).catch(err => console.log(err));
