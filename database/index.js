6a0e746f6e2b080755af175106fc6f53fd293286
Mara <alaneraerts@live.be>
2021-04-11 18:46:55 +0200

    connect to db

---
+++ database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+exports.clientPromise = mongoose.connect('mongodb+srv://admin:WHNzzWyJHlmUEv9c@nogamenolife.eqdfq.mongodb.net/nogame?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then( () => console.log('CONNECT DB SUCCESS')).catch( err => console.log(err));
