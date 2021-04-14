a9d7e16f782acd70db3f145d0ff0c8d7d914a791
Mara <alaneraerts@live.be>
2021-04-15 01:08:32 +0200

    style + fontawesome

--- database/models/blog.model.js
+++ database/models/blog.model.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const blogSchema = schema({
+    title:{type:String ,required:true,unique:true},
     content:{ type:String ,
-        maxLength:[140 , 'message trop long'],
         minLength:[1 , 'message trop court'] ,
         required: [true , 'Champ requis']
     },
+    created: {
+        type: Date,
+        default: Date.now
+    },
     image:{type:String , default:"defaultBlog.jpg"},
-    author:{type:schema.Types.ObjectId,ref:'user', required:true }
+    author:{type:schema.Types.ObjectId,ref:'user', required:true , unique:false }
 })
 
 const Blog = mongoose.model('blog',blogSchema)
