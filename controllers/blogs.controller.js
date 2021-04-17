9268cbffd34c22f733c9366c703ae377d3fdad64
Mara <alaneraerts@live.be>
2021-04-17 02:21:14 +0200

    styles

--- controllers/blogs.controller.js
+++ controllers/blogs.controller.js
@@ -3,7 +3,8 @@ const {findAllBlogs} = require("../queries/blogs.queries");
 exports.getBlogs = async (req , res , next ) =>{
         try{
             const blogs = await findAllBlogs() .populate('author')
-            res.render('admin/blogs/index',{ blogs , currentUser:req.user})
+
+            res.render('admin/blogs/index',{ blogs , currentUser:req.user, })
         }catch (e) {
             next(e)
         }
