6091daf39f4d74d5360310207b326257de66fdc5
Mara <alaneraerts@live.be>
2021-04-21 20:27:19 +0200

    spacing

--- controllers/blogs.controller.js
+++ controllers/blogs.controller.js
@@ -16,53 +16,77 @@ exports.upload = multer({ storage : multer.diskStorage({
 
 exports.getBlogs = async (req , res , next ) =>{
         try{
+
             const blogs = await findAllBlogs() .populate('author')
             res.render('admin/blogs/index',{ blogs , currentUser:req.user, })
+
         }catch (e) {
+
             next(e)
+
         }
     }
 
 exports.createBlogs = async (req,res ,next) => {
     try{
+
         const body = req.body;
         const {filename:image } = req.file;
+
         await sharp(req.file.path)
             .resize(800)
             .webp({quality:90})
             .toFile( path.resolve(req.file.destination,"resized",image))
         fs.unlinkSync(req.file.path);
+
         await createBlog({ ...body,image:req.file.filename, author:req.user._id, created:Date.now() })
         res.redirect('/admin/blogs')
+
     }catch (e){
+
         next(e)
+
     }
 }
 
 exports.deleteBlogs = async (req , res , next) => {
+
     const blogID = req.params.id;
     const blog = await findBlogs(blogID);
+
     const image = blog.image;
     fs.unlink(path.join(__dirname,`../public/images/blogs/resized/${image}`),(err => err && console.error(err)))
     await deleteBlog(blogID);
+
     const blogs = await findAllBlogs().populate('author')
     res.render('admin/blogs/index',{ blogs , currentUser:req.user })
+
 }
 
 exports.getBlog = async (req,res,next) => {
+
     const blogID = req.params.id;
+
     try{
+
        const blog = await findBlogs(blogID) .populate('author');
        res.render('admin/blogs/blog',{ blog , currentUser:req.user});
+
     }catch (e) {
+
        next(e)
+
     }
 }
 
 exports.updateBlogs = async (req,res,next) => {
+
     const blogID = req.params.id;
+
     try{
+
         const body = req.body;
+
         if(req.file){
             const blog = await findBlogs(blogID);
             const oldImage = blog.image;
@@ -70,17 +94,22 @@ exports.updateBlogs = async (req,res,next) => {
             const upImage = req.file.filename;
             body.image = upImage;
         }
+
         const {filename:image } = req.file;
         await sharp(req.file.path)
             .resize(800)
             .webp({quality:90})
             .toFile( path.resolve(req.file.destination,"resized",image))
         fs.unlinkSync(req.file.path);
+
         await updateBlog(blogID,body);
         res.redirect('/admin/blogs/'+blogID);
+
     }catch (e) {
+
         const errors = Object.keys(e.errors).map( key => e.errors[key].message)
         const blog = await findBlogs(blogID);
         res.status(400).render('/blog-form', { errors,blog, isAuthenticated: req.isAuthenticated(), currentUser: req.user });
+
     }
 }
