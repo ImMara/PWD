7f14bc72263ff1232d9eba37a39002ea41c5460f
Mara <alaneraerts@live.be>
2021-04-21 21:16:47 +0200

    final clean

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ require('./config/passport.config');
 app.use(morgan('short'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(index);
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     app.use(errorHandler())
-}else{
-    app.use((err , req , res , next) =>{
+} else {
+    app.use((err, req, res, next) => {
         const code = err.code || 500;
         res.status(code).json({
-            code:code,
-            message:code === 500 ? null : err.message
+            code: code,
+            message: code === 500 ? null : err.message
         });
     })
 }
