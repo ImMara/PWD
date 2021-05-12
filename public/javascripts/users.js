06f23be2f992287012b8a346365fe62580bd4559
Mara <alaneraerts@live.be>
2021-05-12 22:15:44 +0200

    custom select

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -4,15 +4,49 @@ window.onload = () =>{
 
 init = () =>{
 
-    const select = document.querySelector('.users-page .table tr td select')
-    select.onchange = () => {
+    const select = document.querySelector(".selected");
+    const input = document.querySelector(".custom-select select");
 
-        const value = select.value
-        const userID = select.parentElement.parentElement.getAttribute('id')
-        const container = document.querySelector('body')
+    const triggerEvent = (el, type) => {
+        if ("createEvent" in document) {
+            var e = document.createEvent("HTMLEvents");
+            e.initEvent(type, false, true);
+            el.dispatchEvent(e);
+        } else {
+            var e = document.createEventObject();
+            e.eventType = type;
+            el.fireEvent("on" + e.eventType, e);
+        }
+    };
 
-        axios.patch('/admin/users/'+ userID , { role : value })
-            .catch(error => console.error(error))
-    }
+    select.onclick = () => {
+        document.querySelector(".custom-select .drop").classList.toggle("d-block");
+        select.querySelector("svg").classList.toggle("r-180");
+        select.querySelector("svg").classList.toggle("r-180-none");
+    };
 
+    document.querySelectorAll(".custom-select .drop span").forEach(
+        (s) =>
+            (s.onclick = () => {
+                let opt = input.querySelector("option");
+                opt.setAttribute("value", s.getAttribute("data"));
+                opt.innerHTML = s.innerText;
+                document
+                    .querySelector(".custom-select .drop")
+                    .classList.toggle("d-block");
+                select.innerHTML =
+                    s.innerText + '<i class="fa-duotone fa-chevron-down r-180-none"></i>';
+
+                triggerEvent(input, "change");
+            })
+    );
+
+
+    input.onchange = () => {
+            const value = input.value
+            const userID = select.parentElement.parentElement.parentElement.getAttribute('id')
+
+            axios.patch('/admin/users/'+ userID , { role : value })
+                .catch(error => console.error(error))
+    };
 }
